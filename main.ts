69fb24d6f4e910fbe45851a95e5ed39d28af2e9e
Mads Smed <mads.th@hotmail.com>
2025-11-06 16:11:23 +0100

    adds basic movement of the character

--- main.ts
+++ main.ts
@@ -1,9 +1,39 @@
+function characterBoundCheck () {
+    if (currentLocation.x > 4) {
+        currentLocation.x = 4;
+    }
+    if (currentLocation.x < 0) {
+        currentLocation.x = 0;
+    }
+}
 input.onButtonPressed(Button.A, function () {
-    strip.show()
-    strip.showRainbow(0, 0)
+    moveCharacter("left")
 })
-let strip: neopixel.Strip = null
-strip = neopixel.create(DigitalPin.P0, 10, NeoPixelMode.RGB)
+function moveCharacter (direction: string) {
+    if (direction == "right") {
+        led.unplot(currentLocation.x, currentLocation.y)
+        currentLocation.x += 1
+characterBoundCheck()
+        led.plot(currentLocation.x, currentLocation.y)
+    }
+    if (direction == "left") {
+        led.unplot(currentLocation.x, currentLocation.y)
+        currentLocation.x -= 1
+characterBoundCheck()
+        led.plot(currentLocation.x, currentLocation.y)
+    }
+}
+input.onButtonPressed(Button.B, function () {
+    moveCharacter("right")
+})
+let strip = null
+let direction = {
+    LEFT: "left",
+    RIGHT: "right"
+}
+let currentLocation = {x: 2, y: 4}
+led.plot(currentLocation.x, currentLocation.y)
+
 basic.forever(function () {
 	
 })
